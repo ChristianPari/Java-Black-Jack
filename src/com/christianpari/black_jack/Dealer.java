1589e760ee4436e0382fdda7927d43b35264be10
Christian Pari <christianpari@outlook.com>
2021-01-17 05:49:58 -0500

    Began implementing a dealer Hand as well, this way the players have to try and beat the dealers score to win instead of beating each other

--- src/com/christianpari/black_jack/Dealer.java
+++ src/com/christianpari/black_jack/Dealer.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Dealer {
   private final String DEALER = "Dealer";
   private Deck deck;
+  private Hand hand;
+  private int score;
 
   public void newDeck(Deck deck) { this.deck = deck; }
 
@@ -14,14 +16,52 @@ public class Dealer {
     List<Player> players,
     int cardsPerPlayer
   ) {
-    for (var player : players) {
+    for (int playerCounter = 0; playerCounter <= players.size(); playerCounter++) {
       for (int count = 1; count <= cardsPerPlayer; count++) {
         Card card = deck.draw();
-        player.takeCard(card);
+
+        if (playerCounter == 0) { // player 0 is the dealer
+          hand.addCard(card);
+        } else {
+          players.get(playerCounter).takeCard(card);
+        }
       }
     }
   }
 
   public Card giveCard() { return deck.draw(); }
+
+  public boolean stayOrHit(String[] choices) {
+    checkForAce();
+    String decision = choices[Console.getChoice(choices) - 1];
+    if (decision.equalsIgnoreCase("stay")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  private void checkForAce() {
+    for (var card : hand.getCards()) {
+      int cardValue = card.getValue();
+      if (cardValue == 1 || cardValue == 11) {
+        String[] choices = {"1", "11"};
+        int choiceIdx = Console.getChoice(
+          "What would you like your ACE to be valued at?",
+          choices) - 1;
+        if (cardValue != Integer.parseInt(choices[choiceIdx])) {
+          card.changeAceValue();
+        }
+        System.out.println("\n".trim());
+      }
+    }
+  }
+
+  public void clearHand() {
+    hand.emptyHand();
+    score = 0;
+  }
+
+  public int getScore() { return score; }
 
 }
